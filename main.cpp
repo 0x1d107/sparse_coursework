d025122659d9150844fa4fc43c2dd7aa2eba0027
0x1d107 <yablinchik@gmail.com>
2022-12-17 08:51:19 +0500

    single-run program

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(){
     compressed_matrix m(5);
     compressed_matrix L(5);
     compressed_matrix U(5);
-    for(int i=0;i<100;i++){
+    //for(int i=0;i<100;i++){
         int N = 800;
         
 
@@ -33,8 +33,8 @@ int main(){
         U.print_matrix();
         std::cout<<std::endl;
         */
-        double q = 0.01 + i/100.0;
-        std::cout<<q<<' ';
+        double q = 0.20;
+        //std::cout<<q<<' ';
 
         m = compressed_matrix(N);
         b = std::vector<double>(N,1);
@@ -46,13 +46,13 @@ int main(){
         L.solve_L(b,y);
         U.solve_U(y,x);
         auto lu_end = clock();
-        std::cout<<(lu_end - lu_start*1.0)/CLOCKS_PER_SEC<<' ';
+        std::cout<<"LU time:"<<(lu_end - lu_start*1.0)/CLOCKS_PER_SEC<<std::endl;
         x = std::vector<double>(N,0);
         auto bcg_start = clock();
         double r = m.BiCGStab_solve(b,x,1000000);
         auto bcg_end = clock();
-        std::cout<<(bcg_end - bcg_start*1.0)/CLOCKS_PER_SEC<<std::endl;
-        //std::cout<<"Residue norm:"<<r<<std::endl;
-    }
+        std::cout<<"BiCGStab time:"<<(bcg_end - bcg_start*1.0)/CLOCKS_PER_SEC<<std::endl;
+        std::cout<<"Residue norm:"<<r<<std::endl;
+    //}
 
 }
